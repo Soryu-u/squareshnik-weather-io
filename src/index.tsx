400f5644fe09c64f0ceb0983a677ef8ecaa36294
Soryu <collages447@gmail.com>
2023-04-29 13:28:30 +0300

    install and setup react-router

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
-import App from './App'
+import Root from './routes/root/Root'
+import ErrorPage from './error-page'
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Root />,
+        errorElement: <ErrorPage />,
+    },
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
     <React.StrictMode>
-        <App />
+        <RouterProvider router={router} />
     </React.StrictMode>,
 )
---
+++ src/routes/root/Root.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from './Root.module.css'
+
+function Root() {
+    return <div className={styles.main}>hello</div>
+}
+
+export default Root
